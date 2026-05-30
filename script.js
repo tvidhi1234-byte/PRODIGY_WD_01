c39b79287d492e63721ad5fa65b4d402977180d1
tvidhi1234-byte <tvidhi1234@gmail.com>
2026-05-30 18:51:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function () {
+
+    let navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
